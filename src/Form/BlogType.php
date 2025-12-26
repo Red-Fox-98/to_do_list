d35eaa3509c0f0b6fbeee5e7a7505c4e01d7f90d
Евгения Зайцева <e.zaytseva@krutoymedia.ru>
2025-12-26 14:00:18 +0300

    Добавлены entity Tag и TagTransformer, а также отношение ManyToMany

--- src/Form/BlogType.php
+++ src/Form/BlogType.php
@@ -4,14 +4,20 @@ namespace App\Form;
 
 use App\Entity\Blog;
 use App\Entity\Category;
+use App\Form\DataTransformer\TagTransformer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class BlogType extends AbstractType
 {
+    public function __construct(private readonly TagTransformer $transformer)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -24,7 +30,15 @@ class BlogType extends AbstractType
                 'required' => false,
                 'empty_data' => null,
             ])
+            ->add('tags', TextType::class, array(
+                'label' => 'Теги',
+                'required' => false,
+                'by_reference' => false,
+            ))
         ;
+
+        $builder->get('tags')
+            ->addModelTransformer($this->transformer);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
